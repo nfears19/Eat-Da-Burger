d907158b304e4bf348faf6ec85f8a7ec28f5ab1e
Nakiyah Fears <fkiyah@yahoo.com>
2021-03-02 22:07:35 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Eat-Da-Burger
+
+## Description
+This repository containes a burger logger with MySQL, Node, Express, Handlebars and a homemade ORM (yum!). This is a homework assignment from the Case Western Reserve University’s Coding Bootcamp.
---
+++ controllers/burgerController.js
@@ -0,0 +1,54 @@
+const express = require('express');
+
+const router = express.Router();
+
+const burger = require('../models/burger.js');
+
+router.get('/', (req, res) => {
+  burger.all((data) => {
+    const hbsObject = {
+      burgers: data,
+    };
+    console.log(hbsObject);
+    res.render('index', hbsObject);
+  });
+});
+
+router.post('/', (req, res) => {
+  burger.create(['burger_name', 'devoured'], [req.body.burger_name, req.body.devour], (result) => {
+    res.json({ id: result.insertId });
+  });
+});
+
+router.put('/', (req, res) => {
+  const condition = `id = ${req.params.id}`;
+
+  console.log('condition', condition);
+
+  burger.update(
+    {
+      devour: req.body.devour,
+    },
+    condition,
+    (result) => {
+      if (result.changedRows === 0) {
+        return res.status(404).end();
+      }
+      res.status(200).end();
+    }
+  );
+});
+
+router.delete('/', (req, res) => {
+    const condition = `id = ${req.params.id}`;
+  
+    burger.delete(condition, (result) => {
+      if (result.affectedRows === 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      }
+      res.status(200).end();
+    });
+  });
+
+module.exports = router;
